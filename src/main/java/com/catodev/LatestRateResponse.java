950dcc76eed0f6d45a01c84d3bc1867003f241d6
Catriel42 <catopereira72@gmail.com>
2025-11-29 16:44:29 -0400

    Adding DTOs responses to modeling response data

--- src/main/java/com/catodev/LatestRateResponse.java
+++ src/main/java/com/catodev/LatestRateResponse.java
@@ -2,7 +2,7 @@ package com.catodev;
 
 import java.util.Map;
 
-public record StandardRequestResponse(
+public record LatestRateResponse(
    String result,
    String baseCode,
    Map<String, Double> conversionRates
