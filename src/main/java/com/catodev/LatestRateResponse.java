ba4879a72594f1c5f45ae7144a86bb6b02fc54f9
Catriel42 <catopereira72@gmail.com>
2025-11-29 18:17:10 -0400

    fix(mapping): Fixing latest rate response mapping

--- src/main/java/com/catodev/LatestRateResponse.java
+++ src/main/java/com/catodev/LatestRateResponse.java
@@ -1,9 +1,11 @@
 package com.catodev;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.Map;
 
 public record LatestRateResponse(
    String result,
    String baseCode,
-   Map<String, Double> conversionRates
+   @SerializedName("conversion_rates") Map<String, Double> conversionRates
 ) {}
